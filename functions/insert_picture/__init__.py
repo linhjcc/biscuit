80e298179c986c68133084c7b1813f78f1dbda0b
lukelook <lukeluocn@outlook.com>
2023-10-07 20:53:41 +0800

    improve clarity

--- functions/insert_picture/__init__.py
+++ functions/insert_picture/__init__.py
@@ -8,7 +8,6 @@ from baseopensdk.api.base.v1 import (
     AppTableRecord,
     ListAppTableRecordRequest,
     UpdateAppTableRecordRequest,
-    UpdateAppTableRecordResponse,
 )
 from baseopensdk.api.drive.v1 import (
     UploadAllMediaRequest,
@@ -18,30 +17,20 @@ from baseopensdk.api.drive.v1 import (
 picture = Blueprint("insert_picture", __name__)
 
 
-def dataToLineChar(x: str, y: str, data_x: list, data_y: list):
-    """
-    根据提供的两列数据绘制折线图，并保存于本地
-    :param x: x轴名称
-    :param y: y轴名称
-    :param data_x: x轴数据
-    :param data_y: y轴数据
-    :return: 图片存储路径
-    """
-
+def dataToLinePlot(x: str, y: str, data_x: list, data_y: list) -> str:
     assert len(data_x) == len(data_y)
-
+    path = "./static/line_plot.png"
     plt.plot(data_x, data_y, marker="o")
     plt.xlabel(x)
     plt.ylabel(y)
-    plt.savefig("./static/lineChar.png")
+    plt.savefig(path)
     plt.clf()
+    return path
 
-    return "./static/lineChar.png"
 
-
-def upload(APP_TOKEN, TABLE_ID, client, path, record_id):
-    img = Image.open(path, mode="r")
+def upload(APP_TOKEN: str, TABLE_ID: str, client: BaseClient, path: str, record_id: str) -> None:
     imgByteArr = io.BytesIO()
+    img = Image.open(path, mode="r")
     img.save(imgByteArr, format="PNG")
     imgByteArr = imgByteArr.getvalue()
 
@@ -49,7 +38,7 @@ def upload(APP_TOKEN, TABLE_ID, client, path, record_id):
         UploadAllMediaRequest.builder()
         .request_body(
             UploadAllMediaRequestBody.builder()
-            .file_name("test2.png")
+            .file_name("plot.png")
             .parent_type("bitable_image")
             .parent_node(APP_TOKEN)
             .size(len(imgByteArr))
@@ -58,11 +47,9 @@ def upload(APP_TOKEN, TABLE_ID, client, path, record_id):
         )
         .build()
     )
-
     response = client.drive.v1.media.upload_all(request)
 
     file_token = response.data.file_token
-
     request = (
         UpdateAppTableRecordRequest.builder()
         .table_id(TABLE_ID)
@@ -74,17 +61,21 @@ def upload(APP_TOKEN, TABLE_ID, client, path, record_id):
         )
         .build()
     )
+    _ = client.base.v1.app_table_record.update(request)
 
-    response: UpdateAppTableRecordResponse = client.base.v1.app_table_record.update(request)
 
-
-def insert_picture(APP_TOKEN, PERSONAL_BASE_TOKEN, TABLE_ID, field1, field2):
+def insert_picture(
+    APP_TOKEN: str,
+    PERSONAL_BASE_TOKEN: str,
+    TABLE_ID: str,
+    field1: str,
+    field2: str,
+) -> str:
     # 1. 构建client
-    client: BaseClient = BaseClient.builder().app_token(APP_TOKEN).personal_base_token(PERSONAL_BASE_TOKEN).build()
+    client = BaseClient.builder().app_token(APP_TOKEN).personal_base_token(PERSONAL_BASE_TOKEN).build()
 
     # 2. 遍历记录
     list_record_request = ListAppTableRecordRequest.builder().page_size(100).table_id(TABLE_ID).build()
-
     list_record_response = client.base.v1.app_table_record.list(list_record_request)
     records = getattr(list_record_response.data, "items", [])
 
@@ -92,20 +83,14 @@ def insert_picture(APP_TOKEN, PERSONAL_BASE_TOKEN, TABLE_ID, field1, field2):
     data_x = []
     data_y = []
     for record in records:
-        record_id, fields = record.record_id, record.fields
-        print(fields)
-        x = fields.get(field1, {})
-        y = fields.get(field2, {})
-
+        x = record.fields.get(field1, None)
+        y = record.fields.get(field2, None)
         if x and y:
             data_x.append(int(x))
             data_y.append(int(y))
 
-    print(data_x)
-    print(data_y)
-
     # 4. 画图
-    picture_path = dataToLineChar(field1, field2, data_x, data_y)
+    picture_path = dataToLinePlot(field1, field2, data_x, data_y)
 
     # 5.上传
     upload(APP_TOKEN, TABLE_ID, client, picture_path, records[0].record_id)
